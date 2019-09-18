3a18cd42d4cfbb816b0e8c8936272803ab2361a0
MrBurp <andreass.ziegler@gmail.com>
2019-09-18 13:18:42 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Router from "./components/Router";
 import "./sass/style.scss";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 render(<Router />, document.querySelector("#main"));
