e912199051afb73cdc1dc83252f1611664da5fcc
MrBurp <andreass.ziegler@gmail.com>
2019-09-18 10:53:34 +0200

    react.fragment wrap

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>What to do?</h1>
+        <span className="tagline">This could be Article list.</span>
+      </React.Fragment>
+    );
+  }
+};
+
+export default Header;
